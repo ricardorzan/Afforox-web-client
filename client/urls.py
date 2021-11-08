2b87215f059aa7371bb3ec0beba3a6236d03137b
ricardorzan <ricardorzan@gmail.com>
2021-11-08 11:05:18 -0600

    - Modulo about

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Homepage, name='index'),
+    path('', views.Homepage, name='home'),
+    path('about/', views.About, name='about'),
 ]
